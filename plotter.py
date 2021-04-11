1f7005549947b25fa5ba92f6321e2d22cce8377f
murfel <murfel@users.noreply.github.com>
2021-04-11 15:35:45 +0200

    output statistics in plotter

--- plotter.py
+++ plotter.py
@@ -1,7 +1,15 @@
 import matplotlib.pyplot as plt
+import statistics
 
-xs = list(map(int, input().split()))
-plt.plot(xs)
-plt.xlabel('DeleteMin index')
-plt.ylabel('Rank Error')
+xs = [1, 2, 3]
+print(len(xs))
+
+print('abs avg', statistics.mean(map(abs, xs)))
+print('neg avg', statistics.mean(filter(lambda x: x < 0, xs)))
+print('pos avg', statistics.mean(filter(lambda x: x > 0, xs)))
+print('num zeros', xs.count(0))
+
+plt.plot(xs, 'bo', markersize=1)
+plt.xlabel('DeleteMin operation serial number')
+plt.ylabel('Rank Error (expected - actual)')
 plt.show()
